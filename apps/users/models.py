61620fe898fc73387af5d70acd934460cbfc470d
Abayomi Olowu <aayobam@gmail.com>
2024-03-18 10:05:55 +0100

    added default vehicle type and make as choice fields

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -11,10 +12,13 @@ class CustomUser(BaseModel, PermissionsMixin, AbstractBaseUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True, max_length=100)
-    gender = models.CharField(default="male", choices=choices.GENDER, max_length=10)
+    gender = models.CharField(
+        default="male", choices=choices.GENDER, max_length=10)
     phone_no = models.CharField(max_length=50, blank=True)
-    document_type = models.CharField(max_length=100, choices=choices.DOCUMENT_TYPES)
-    document = models.FileField(upload_to="documents/", help_text=_("upload a legal document that truely identifies you for verification."), blank=True, null=True)
+    document_type = models.CharField(
+        max_length=100, choices=choices.DOCUMENT_TYPES)
+    document = models.FileField(upload_to="documents/", help_text=_(
+        "upload a legal document that truely identifies you for verification."), blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
@@ -31,7 +35,7 @@ class CustomUser(BaseModel, PermissionsMixin, AbstractBaseUser):
         return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
 
     def __str__(self):
-        return f"{self.get_full_name()}"
+        return f"{self.email}"
 
     def get_absolute_url(self):
         return reverse("user_detail", kwargs={"id": self.id})
