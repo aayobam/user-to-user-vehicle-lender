383c4ac41f00f6afcbae9b9f3f7295af50fe91ef
Abayomi Olowu <aayobam@gmail.com>
2024-03-24 11:27:06 +0100

    addedminore changes

--- core/views.py
+++ core/views.py
@@ -1,18 +1,24 @@
-from typing import Any
-from django.views.generic import TemplateView
-
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.views import generic
+from django.db.models import Q
+from apps.vehicles.models import Vehicle
 from apps.common.choices import VEHICLE_MAKES, VEHICLE_TYPES
 
 
-class HomeView(TemplateView):
+class HomeView(generic.ListView):
+    model = Vehicle
     template_name = "home.html"
+    context_object_name = "vehicles"
+    paginate_by = 20
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+    def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["VEHICLE_TYPES"] = VEHICLE_TYPES
         context["VEHICLE_MAKES"] = VEHICLE_MAKES
+        context["VEHICLE_TYPES"] = VEHICLE_TYPES
+        context["total_available_vehicles"] = self.get_queryset().count()
         return context
 
 
-class AboutView(TemplateView):
+class AboutView(generic.TemplateView):
     template_name = "about.html"
